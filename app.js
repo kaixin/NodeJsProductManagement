eeb3b0e5611bd8bfaadf47117c14e37e31429467
Jiangxia Nan <jiangxia.nan@oracle.com>
2019-03-30 16:10:51 +0800

    login page: verify and login

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var http = require('http');
+var bodyParser = require('body-parser');
+
+var MongoClient = require('mongodb').MongoClient;
+var dbURL = "mongodb://127.0.0.1:27017";
+var dbName = "productmanagement"
 
 var app = new express();
 
 var server = http.createServer(app);
 
 server.listen(8000, '127.0.0.1');
+
+//设置bodyParser中间件
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //使用ejs模板引擎，默认找views这个目录
 app.set('view engine', 'ejs');
@@ -15,6 +24,27 @@ app.use(express.static('public'));
 
 app.get('/login', function(rep, res) {
   res.render('login');
+});
+
+app.post('/doLogin', function(rep, res) {
+  MongoClient.connect(dbURL, function(err, client) {
+    var db = client.db(dbName);
+    if(err) {
+      console.log('连接数据库失败');
+    }
+
+    var result = db.collection("user").find(rep.body);
+    result.toArray(function(err, data) {
+      if(data.length > 0) {
+        console.log('登陆成功');
+        res.redirect('/product');
+      }else {
+        console.log('登陆失败');
+        res.send("<script>alert('登陆失败'); window.location.href='/login'</script>");
+      }
+    });
+  });
+  console.log(rep.body);  
 });
 
 app.get('/product', function(rep,res) {
